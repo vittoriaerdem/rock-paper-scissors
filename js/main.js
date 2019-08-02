b9a60be780507d0b04990c265e5192995e2aa5c6
Vittoria Erdem <vittoria@Vittorias-MacBook-Pro.local>
2019-08-02 12:50:33 -0500

    render results

--- js/main.js
+++ js/main.js
@@ -6,10 +6,71 @@ const shootAudio = new Audio('http://soundbible.com/mp3/shooting_star-Mike_Koeni
 
 
 /*----- cached element references -----*/
+const scoreEls = {
+    p: document.getElementById('p-score'),
+    t: document.getElementById('t-score'),
+    c: document.getElementById('c-score')
+};
 
+const resultEls = {
+    p: {
+        borderEl: document.getElementById('p-result'),
+        imgEl: document.querySelector('#p-result img')
+    },
+    c: {
+        borderEl: document.getElementById('c-result'),
+        imgEl: document.querySelector('#c-result img')
+    },
+};
 
+const repsLookup = {
+    r: {
+        imgUrl: 'imgs/rock.png', 
+        beats: 's'
+    },
+    p: {
+        imgUrl: 'imgs/paper.png', 
+        beats: 'r'
+    },
+    s: {
+        imgUrl: 'imgs/scissors.png', 
+        beats: 'p'
+    },
+};
 /*----- event listeners -----*/
-
+document.querySelector('main button')
+    .addEventListener('click', playRound);
 
 /*----- functions -----*/
-hi
+init ();
+
+function init(){
+    scores = {
+        p: 0, 
+        t: 0, 
+        c: 0
+    };
+    results = {
+        p: 'r', 
+        c: 'r'
+    };
+    winner = null; // 'p', 't', 'c' 
+    render();
+}
+
+function render(){
+// render scores
+    for(let score in scores){
+        scoreEls[score].textContent = scores[score];
+    }
+// render results
+    for(let result in results){
+        resultEls[result].borderEl.style.borderColor = 
+            winner === result ? 'grey' : 'white';
+        resultEls[result].imgEl.src = 
+    }
+}
+
+function playRound(){
+    console.log('clicked');
+}
